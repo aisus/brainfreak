ce26fb9c3f0deb8e3b5cf93536d9432ba4958af5
a.belousov <a.belousov@foodplexgroup.ru>
2022-06-10 11:13:33 +0300

    tmp

--- BrainFreak/Parser/Parser.fs
+++ BrainFreak/Parser/Parser.fs
@@ -1,6 +1,19 @@
 ﻿module BrainFreak.Parser
 
 open BrainFreak.Core
+open FParsec
+
+let right: Parser<_, Operator> = pchar '>'
+let left: Parser<_, Operator> = pchar '<'
+let inc: Parser<_, Operator> = pchar '+'
+let dec: Parser<_, Operator> = pchar '-'
+let write: Parser<_, Operator> = pchar '.'
+let read: Parser<_, Operator> = pchar ','
+let loopStart: Parser<_, Operator> = pchar '['
+let loopEnd: Parser<_, Operator> = pchar ']'
+
+let loop: Parser<_, Operator> =
+    loopStart >>. manyCharsTill anyChar loopEnd
 
 let parseCharToOperator (input: char) =
     LanguagePrimitives.EnumOfValue<char, Operator> input
--- BrainFreak/Program.fs
+++ BrainFreak/Program.fs
@@ -2,21 +2,23 @@
 
 open BrainFreak.Core
 open BrainFreak.Parser
+open FParsec
 
 [<EntryPoint>]
 let main argv =
     let test = Seq.toList "+++.>++++.>+++++."
 
-    let input =
-        test
-        |> Seq.map (parseCharToOperator)
-        |> Seq.toList
-
-    let state =
-        { buffer = Array.zeroCreate 10
-          pointer = 0 }
-
-    Interpreter.execute input state
-    printfn "\n"
-    printfn "%A" state.buffer
+//    let input =
+//        test
+//        |> Seq.map (parseCharToOperator)
+//        |> Seq.toList
+//
+//    let state =
+//        { buffer = Array.zeroCreate 10
+//          pointer = 0 }
+//
+//    Interpreter.execute input state
+//    printfn "\n"
+//    printfn "%A" state.buffer
+    let result = run left '<'
     0
